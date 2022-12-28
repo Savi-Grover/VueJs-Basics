5383f12c57ea21230efbf5fc7d2b618587bae7f4
Savi Grover <savig447@gmail.com>
2022-12-28 17:23:49 -0500

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
  <template v-for="name in names" :key="name">
-    <h2 v-if="name === 'bruce'">{{ name }}</h2>
+  <h2 v-if="name === 'bruce'">{{ name }}</h2>
+  
   </template>
+  <h2>Add method - {{ add(10,15,20) }}</h2>
+  <h2>Multiply method - {{ multiply(10) }}</h2>
 </template>
 
 <script>
@@ -9,10 +12,22 @@ export default {
   name: 'App',
   data(){
     return {
+      baseMultplier: 5,
       names: ['bruce','clark','diana'],
     }
   },
-}
+
+  methods: {
+    add(a,b,c){
+      return a +b +c
+
+    },
+    multiply(num){
+      return num*this.baseMultplier
+    }
+  },
+  }
+
 </script>
 
 <style>
