57774d97438c4f6fa49d7cdbf3e99242490bbcac
Savi Grover <savig447@gmail.com>
2022-12-27 23:58:38 -0500

    binding classes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
   <div>
     My name is {{name}} and I am {{ age }}  years young.
     <div v-text="channel"></div>
+    <h2 v-bind:id="headingId">Heading</h2>
+    <button v-bind:disabled="isDisabled">checkButton</button>
+    <h2 class="underline">Under lined text from style class</h2>
+    <h2 v-bind:class="Status">Status</h2>
+    <h2 class="underline" v-bind:class="Status">2 classes</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Sold out/new</h2>
+    <h2 v-bind:class="['new','promoted']" >2 classes </h2>
+    <h2 v-bind:class="[isPromoted && 'promoted',isSoldout ? 'sold-out': 'new']" >Conditional Array</h2>
+    <h2 v-bind:class="{
+      promoted : isPromoted,
+      new : !isSoldout,
+      'sold-out': isSoldout
+
+    }">object conditional classes</h2>
   </div>
   
 </template>
@@ -12,11 +27,15 @@ export default {
     return {
       name: "Peter",
       age: "29",
-      channel: "codeInvasion",
+      headingId: '3',
+      isDisabled: true,
+      Status:"danger",
+      isPromoted: true,
+      isSoldout:true,
+      isNew: true
     };
   }
 }
-
 </script>
 
 <style>
@@ -27,5 +46,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline{
+  text-decoration: underline;
+}
+
+.promoted{
+  font-style: italic;
+}
+
+.new{
+  color: olivedrab;
+}
+
+.sold-out{
+  color: red;
 }
 </style>
